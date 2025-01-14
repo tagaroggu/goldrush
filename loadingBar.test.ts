08af68db9625e017b6bb93762dcb3a5b003b4a64
tagaroggu <58315587+tagaroggu@users.noreply.github.com>
2025-01-14 03:46:19 -0600

    chore: format and lint

--- loadingBar.test.ts
+++ loadingBar.test.ts
@@ -1,5 +1,7 @@
 import { loadingBar } from './loadingBar.ts';
 
-loadingBar(Array.from({ length: 10}).map((_, idx) => {
-	return new Promise((res) => setTimeout(() => res(null), (idx + 1) * 1000));
-}))
+loadingBar(
+	Array.from({ length: 10 }).map((_, idx) => {
+		return new Promise((res) => setTimeout(() => res(null), (idx + 1) * 1000));
+	}),
+);
